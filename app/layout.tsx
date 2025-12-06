6cb4c4860de7181327bf86e413877dc2eebad635
hopkins385 <stadhouders.sven@gmail.com>
2025-12-06 13:00:22 +0100

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ConfirmDialogProvider>{children}</ConfirmDialogProvider>
         <Toaster position="top-center" richColors />
       </body>
